da4fedc4c4c5c7c299158850f9c38c51ab03b2df
Makis Tracend <makis.tracend@gmail.com>
2015-01-28 22:28:32 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Assetorium CLI
+
+# 1. Install the main executable
+npm install -g assetorium-cli
+
+#2. Setup assetorium (only needed once)
+assetorium setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+assetorium -h
